648e4cba2706959efd28339c095a404b44f37b3f
Anisur Rahman Rifat <69327775+ar-riifat@users.noreply.github.com>
2023-02-21 03:28:23 +0600

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\abdul\\development\\sdks\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Study Material\\8th Semester\\3rd Year Project\\authentication" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\abdul\\development\\sdks\\flutter"
+  "PROJECT_DIR=D:\\Study Material\\8th Semester\\3rd Year Project\\authentication"
+  "FLUTTER_ROOT=C:\\Users\\abdul\\development\\sdks\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Study Material\\8th Semester\\3rd Year Project\\authentication\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Study Material\\8th Semester\\3rd Year Project\\authentication"
+  "FLUTTER_TARGET=D:\\Study Material\\8th Semester\\3rd Year Project\\authentication\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Study Material\\8th Semester\\3rd Year Project\\authentication\\.dart_tool\\package_config.json"
+)
